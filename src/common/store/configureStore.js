fffd9e75478373de09b0b1d4f76d77925748e192
Svitlana Shepitsena <chicagobusinessintelligence1@gmail.com>
2015-12-30 13:38:10 -0600

    Redux DevTools

--- src/common/store/configureStore.js
+++ src/common/store/configureStore.js
@@ -41,6 +41,7 @@ const middlewareBuilder = () => {
                     createHistory
                 }),
                 chromedevtools
+                //devTools()
             ]
         }
     } else {
